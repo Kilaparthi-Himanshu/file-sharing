07626bb4888b742b0884804515793a10e87ac247
Himanshu <himanshunani9@gmail.com>
2025-03-23 21:24:24 +0530

    Fixed Bug on mobile devices

--- app/components/ButtonsUi.tsx
+++ app/components/ButtonsUi.tsx
@@ -11,10 +11,11 @@ export const ButtonsUi = () => {
     }
 
     return (
-        <div className='w-120 h-40 flex gap-5 items-center justify-around font-mono'>
+        <div className='w-120 max-lg:w-60 max-lg:gap-2 h-40 flex items-center justify-around font-mono'>
             <button className='border border-neutral-600 w-40 h-15 rounded-lg text-white text-xl hover:bg-neutral-800 transition-[background,scale] cursor-pointer active:scale-98' onClick={() => handleRedirect("send")}>
                 Send
             </button>
+
             <button className='border border-neutral-600 w-40 h-15 rounded-lg text-white text-xl hover:bg-neutral-800 transition-[background,scale] cursor-pointer active:scale-98' onClick={() => handleRedirect("receive")}>
                 Receive
             </button>
--- app/send/page.tsx
+++ app/send/page.tsx
@@ -4,8 +4,8 @@ import { FileInput } from '../components/FileInput';
 export default function Send() {
 
     return (
-        <div className='bg-black h-screen w-screen flex flex-col gap-15 items-center justify-center p-4 text-white'>
-           <div className='border border-neutral-500 w-120 h-auto rounded-xl flex flex-col items-center px-8 py-8'>
+        <div className='bg-black h-[100dvh] w-screen flex flex-col gap-15 items-center justify-center p-4 text-white'>
+           <div className='border border-neutral-500 w-120 h-auto rounded-xl flex flex-col items-center px-8 py-8 max-lg:scale-75'>
                 <FileInput />
            </div>
         </div>

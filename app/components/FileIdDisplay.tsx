d61f7dbd11d4d2106bc45accfadf577b7336370c
Himanshu <himanshunani9@gmail.com>
2025-03-25 23:58:03 +0530

    Fixed some styling with ID Display

--- app/components/FileIdDisplay.tsx
+++ app/components/FileIdDisplay.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 export const FileIdDisplay = ({ fileId }: { fileId: string }) => {
     
   return (
-    <div className='absolute flex flex-col max-lg:flex-row items-center w-max h-max top-0 p-4 py-0 rounded-xl'>
-        <span className='tracking-normal font-bold p-2 border text-4xl border-neutral-500 px-12 rounded-b-xl border-t-0 max-lg:border-0 max-lg:px-0 max-lg:ml-4'>
-            ID:
+    <div className='absolute flex flex-col max-lg:flex-row items-center w-max h-max top-0 p-4 py-0 rounded-xl max-lg:bg-black'>
+        <span className='tracking-normal lg:w-50 text-center font-bold p-2 border text-4xl border-neutral-500 px-12 rounded-b-xl border-t-0 max-lg:border-0 max-lg:px-0 max-lg:ml-4 bg-black'>
+            ID
         </span>
-        <div className='border border-neutral-500 rounded-b-xl border-t-0 px-3 py-2 text-center flex justify-center tracking-[8px] text-3xl max-lg:border-0'>
+        <div className='border border-neutral-500 rounded-b-xl border-t-0 px-3 py-2 text-center flex justify-center tracking-[8px] text-3xl max-lg:border-0 bg-black'>
             {fileId}
         </div>
     </div>

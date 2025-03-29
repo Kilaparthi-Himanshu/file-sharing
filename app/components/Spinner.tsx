ce779393e403d6f61ccff8d5ac8e300863a72dee
Himanshu <himanshunani9@gmail.com>
2025-03-29 13:58:36 +0530

    Chagned the spinner

--- app/components/Spinner.tsx
+++ app/components/Spinner.tsx
@@ -6,8 +6,14 @@ import { AnimatePresence, motion } from 'framer-motion';
 export const Spinner = () => {
     return (
         <motion.div className='bg-[rgba(43,43,43,0.3)] absolute w-full h-full flex items-center justify-center py-9 px-2 z-1000 left-0 top-0' initial={{ opacity: 0}} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.1 }}>
-            <div className="flex items-center justify-center h-screen">
-                <div className="w-20 h-20 border-4 border-white border-t-transparent rounded-full animate-spin" />
+            <div className="flex-col gap-4 w-full flex items-center justify-center">
+                <div
+                    className="w-20 h-20 border-4 border-transparent text-neutral-400 text-4xl animate-spin flex items-center justify-center border-t-neutral-400 rounded-full"
+                >
+                    <div
+                    className="w-16 h-16 border-4 border-transparent text-neutral-200 text-2xl animate-spin flex items-center justify-center border-t-neutral-200 rounded-full"
+                    ></div>
+                </div>
             </div>
         </motion.div>
     );

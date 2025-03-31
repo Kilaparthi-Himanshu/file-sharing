1c13a1677160c63b2a8a40d2a277dacec5e4b066
Himanshu <himanshunani9@gmail.com>
2025-03-31 17:46:57 +0530

    Adde animation to send page

--- app/send/page.tsx
+++ app/send/page.tsx
@@ -6,6 +6,7 @@ import { ModeSelect } from '../components/ModeSelect';
 import { selectModeAtom } from '../Atoms/atoms';
 import { useAtom } from 'jotai';
 import { TextInput } from '../components/TextInput';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function Send() {
     const [mode, setMode] = useAtom(selectModeAtom);
@@ -13,8 +14,22 @@ export default function Send() {
     return (
         <div className='bg-black h-[100dvh] w-screen flex flex-col gap-15 items-center justify-center p-4 text-white overflow-hidden'>
            <div className='border border-neutral-500 w-120 h-auto rounded-xl flex flex-col items-center px-8 py-8 max-lg:w-80'>
-                {mode === "File" ? <FileInput /> : <TextInput />}
-           </div>
+                <AnimatePresence mode="wait">
+                    <motion.div
+                        key={mode}
+                        initial={{ opacity: 0, y: -20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: -20 }}
+                        transition={{
+                            duration: 0.3,
+                            ease: "easeInOut"
+                        }}
+                        className='w-full'
+                    >
+                        {mode === "File" ? <FileInput /> : <TextInput />}
+                    </motion.div>
+                </AnimatePresence>
+            </div>
            <ModeSelect />
         </div>
     );

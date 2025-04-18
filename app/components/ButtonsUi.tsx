3895938240f382bc577767063c7127cb333cd9a7
Himanshu <himanshunani9@gmail.com>
2025-04-18 11:10:55 +0530

    Removed the tooltip and prepared for Room developement

--- app/components/ButtonsUi.tsx
+++ app/components/ButtonsUi.tsx
@@ -29,37 +29,9 @@ export const ButtonsUi = () => {
             </div>
 
             <div className='relative'>
-                <div
-                    onMouseEnter={() => setIsVisible(true)} 
-                    onMouseLeave={() => setIsVisible(false)} 
-                    onTouchStart={() => setIsVisible(true)}
-                >
-                    <button className='border border-neutral-600 w-40 h-15 rounded-lg text-white text-xl hover:bg-neutral-800 transition-[background,scale] cursor-pointer active:scale-98 disabled:bg-neutral-800 disabled:active:scale-100 opacity-70' onClick={() => handleRedirect('room')} disabled>
-                        Room
-                    </button>
-                </div>
-
-                <AnimatePresence>
-                {isVisible && (
-                    <motion.div
-                    className="absolute top-[90%] left-1/2 transform -translate-x-1/2 z-50 pointer-events-none"
-                    initial={{ opacity: 0, y: 0 }}
-                    animate={{ opacity: 1, y: 10 }}
-                    exit={{ opacity: 0, y: 0 }}
-                    transition={{ duration: 0.2, ease: "easeInOut" }}
-                    >
-                        <SmoothCorners
-                            corners="12, 3"
-                            borderRadius="20px"
-                            className="bg-neutral-900 p-4 text-white text-xl font-mono"
-                            {...{} as any}
-                        >
-                            Upcoming!
-                        </SmoothCorners>
-                    </motion.div>
-                )}
-                </AnimatePresence>
-                {/* This line is for tesing the branch of feature/room */}
+                <button className='border border-neutral-600 w-40 h-15 rounded-lg text-white text-xl hover:bg-neutral-800 transition-[background,scale] cursor-pointer active:scale-98' onClick={() => handleRedirect('room')}>
+                    Room
+                </button>
             </div>
         </div>
     );
